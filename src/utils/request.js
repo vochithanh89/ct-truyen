0818b6987a48515edde6448756174e3a17b744d3
vochithanh89 <61690947+vochithanh89@users.noreply.github.com>
2022-07-24 09:14:26 +0700

    remove console.log

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'https://nettruyen-api.herokuapp.com',
+    // baseURL: 'https://nettruyen-api.herokuapp.com',
+    baseURL: 'http://localhost:8080',
 });
 
 export const axiosGet = async (path, options) => {
